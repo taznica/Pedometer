f73f6d5038e6d184774f9e2eb4de85ea172fd087
ttsutchi <tailegog@gmail.com>
2018-05-13 00:38:27 +0900

    [Add] getSteps() and label

--- Pedometer/ViewController.swift
+++ Pedometer/ViewController.swift
@@ -7,12 +7,19 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+
+    @IBOutlet var stepsLabel: UILabel!
+
+    let pedometer: CMPedometer = CMPedometer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        getSteps()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +27,20 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func getSteps() {
+        if !(CMPedometer.isStepCountingAvailable()) {
+            return
+        }
 
+        pedometer.startUpdates(from: Date(), withHandler: { (data: CMPedometerData?, error) in
+            DispatchQueue.main.async {
+                if let e = error {
+                    print(e.localizedDescription)
+                }
+                let steps = data!.numberOfSteps
+                self.stepsLabel.text = steps.stringValue
+            }
+        })
+    }
 }
 
